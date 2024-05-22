e4f49b7b7fd0c08e067ad4f4f05fc71c0920d8b5
WhiskeyLord <zerk3000@yandex.com>
2024-05-22 15:27:01 +0300

    Updates

---
+++ precache-manifest.cfef16cc7f5d5d5a24ec58503aee7c97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496b2dbcd299827146281d0576cb06dc",
+    "url": "/spaproj/index.html"
+  },
+  {
+    "revision": "1cc60883d925744db708",
+    "url": "/spaproj/static/css/main.feead368.chunk.css"
+  },
+  {
+    "revision": "0da2e4c32bd32b83f731",
+    "url": "/spaproj/static/js/2.9e272536.chunk.js"
+  },
+  {
+    "revision": "99562942502e41af72fc73d84afe4332",
+    "url": "/spaproj/static/js/2.9e272536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cc60883d925744db708",
+    "url": "/spaproj/static/js/main.aeedd60d.chunk.js"
+  },
+  {
+    "revision": "7075ab2c29e41e296b81",
+    "url": "/spaproj/static/js/runtime-main.cee53759.js"
+  }
+]);
